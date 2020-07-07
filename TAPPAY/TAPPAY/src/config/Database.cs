18afa3da88705add767d14c3e726573c7fa3d757
Brunoalbino97 <bruno-albino@hotmail.com>
2020-07-06 21:17:21 -0300

    Criado clase de banco de dados

---
+++ TAPPAY/TAPPAY/src/config/Database.cs
@@ -0,0 +1,43 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TAPPAY.src.config
+{
+    class Database
+    {
+        private MySqlConnection connection;
+        private string connString;
+        private MySqlCommand command;
+        public Database()
+        {
+
+        }
+        public Database(string connString)
+        {
+            this.connString = connString;
+            this.connection = new MySqlConnection(this.connString);
+            this.command = connection.CreateCommand();
+        }
+
+        //Aqui você substitui pelos seus dados
+        //string connString = "Server=localhost;Database=test;Uid=usuario;Pwd=senha";
+        //var connection = new MySqlConnection(this.connString);
+        //var command = connection.CreateCommand();
+
+        //try
+        //{
+        //    connection.Open();
+        //    command.CommandText = "INSERT INTO TABELA1 (CAMPO1) VALUES ('VALOR1')";
+        //    command.ExecuteNonQuery();
+        //}
+        //finally
+        //{
+        //    if(connection.State == ConnectionState.Open)
+        //        connection.Close();            
+        //}
+    }
+}
